7d6fa3387db7ba9da592f4dcaeb9e62deb9ec222
root <root@node4.(none)>
2014-11-17 02:12:28 +0800

    run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,9 @@
-$JAVA_HOME/bin/javac -d bin/  ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
+if [ ! -d "../bin" ]; then
+  mkdir ../bin
+fi
+$JAVA_HOME/bin/javac -d ../bin  ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
 echo 'compiling is over'
-nohup $JAVA_HOME/bin/java ../bin/ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
+nohup $JAVA_HOME/bin/java -cp  ../bin ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
 OHSUMED/OHSUMED/QueryLevelNorm/Fold4/train.txt  -test  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold4/test.txt \
 -validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold4/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
 echo 'It is done'
