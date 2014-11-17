fadb1067268ed5966bfc272c9c69bcf07f86f239
root <root@node6.(none)>
2014-11-17 11:18:41 +0800

    ssh.

--- src/run.sh
+++ src/run.sh
@@ -4,8 +4,8 @@ fi
 $JAVA_HOME/bin/javac -d ../bin  ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
 echo 'compiling is over'
 nohup $JAVA_HOME/bin/java -cp  ../bin ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
-OHSUMED/OHSUMED/QueryLevelNorm/Fold4/train.txt  -test  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold4/test.txt \
--validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold4/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
+OHSUMED/OHSUMED/QueryLevelNorm/Fold3/train.txt  -test  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold3/test.txt \
+-validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold3/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
 echo 'It is done'
 
 
