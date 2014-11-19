569bef00d041d5f6db2abc4e8a8a9c2c39380b30
root <root@node5.(none)>
2014-11-19 12:03:33 +0800

    make some change in run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,15 +1,9 @@
 if [ ! -d "../bin" ]; then
   mkdir ../bin
 fi
-if [ ! -d "../output/inLearningMatrixV" ]; then
- mkdir ../output/inLearningMatrixV
-fi
-if [ ! -d "../output/afterLearningMatrixV" ]; then
- mkdir ../output/afterLearningMatrixV
-fi
 javac -d ../bin  ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
 echo 'compiling is over'
-nohup $JAVA_HOME/bin/java -cp  ../bin ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
+nohup java -cp  ../bin ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
 OHSUMED/OHSUMED/QueryLevelNorm/Fold5/train.txt  -test  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold5/test.txt \
 -validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold5/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
 echo 'It is done'
