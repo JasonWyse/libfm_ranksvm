747d730774d74414b7bcbdbc05fad31423f61d37
JasonWyse <timeismoneyzyg@163.com>
2014-11-28 02:50:58 +0800

    rum.sh

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@ javac -d ../bin  ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
 echo 'compiling is over'
 nohup java -cp  ../bin ict/edu/learning/logisticRankSVM/LogisticRankSVM -train ../data/\
 OHSUMED/OHSUMED/QueryLevelNorm/Fold1/train.txt  -test  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/test.txt \
--validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/vali.txt -nThread 17  -maxIterations 100
+-validate  ../data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/vali.txt -nThread 16  -maxIterations 100 \
  -output_interval 3 -learningRate 0.00000001 &
 echo 'It is done'
 
